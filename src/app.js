b0058f5f57974ef6ab3dd0bc44b3378122c7b7e8
caelidh <ceilidhliddell@gmail.com>
2023-01-16 18:15:31 -0800

    Reformatted HTML & JS

--- src/app.js
+++
@@ -1,7 +0,0 @@
-function showTemperature(response){
-console.log(response.temperature);
-}
-
-let apiKey = "1d3t1f02a4o0e7ef8cc4fb4cbdca4d5e";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Eureka&key=${apiKey}&units=imperial`;
-axios.get(apiUrl).then(showTemperature)
